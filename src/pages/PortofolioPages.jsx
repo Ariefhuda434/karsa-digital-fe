e4d2b0aeaca7bdfea001f0fef445366c8567d24b
Muhammad Arief Huda <ariefhuda434@gmail.com>
2026-05-31 00:55:33 +0700

    initial addon

--- src/pages/PortofolioPages.jsx
+++ src/pages/PortofolioPages.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useState, useEffect } from 'react'
 import { ArrowRight, X, ExternalLink, Loader2, AlertTriangle } from 'lucide-react'
 import Navbar from '../components/Navbar'
